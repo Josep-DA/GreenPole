eedbbbef9b99883869098d3aa63375c91930bc38
joseph <ahouantojoseph@gmail.com>
2023-02-18 00:57:04 -0500

    18 fév. 2023 - 00:57 - configuration du système pour l'application centrale

--- APP/setup.py
+++ APP/setup.py
@@ -1,21 +1,14 @@
 def start_app(deployment: str = ''):
     from flask import url_for
-    from .setups.app_setup import AppSetup
+    from .setups import app_setup
     from .setups.server_setup import ServerSetup
     from .main import bp as main_bp
     from .recherches import bp as recherches_bp
-    
-
-    # Create app
-    app_setup = AppSetup('main.root')
 
     # Blueprints
     app_setup.register_blueprint(main_bp)
 
     app_setup.register_blueprint(recherches_bp)
-    @app_setup.app.route('/recherches')
-    def recherches():
-        return app_setup.redirect(url_for('recherches.home'))
 
 
     # Create server & Link app to server
