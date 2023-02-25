e5801c9ac9b8055fb1a1b932d7b1ce65e5acdf5a
joseph <ahouantojoseph@gmail.com>
2023-02-25 18:05:32 -0500

    25 fév 2023 - 18:06 - adapter le code pour pythonanywhere

--- APP/setup.py
+++ APP/setup.py
@@ -1,23 +1,26 @@
+from flask import url_for
+from .setups import app_setup
+from .setups.server_setup import ServerSetup
+from .main import bp as main_bp
+from .recherches import bp as recherches_bp
+from .blog import bp as blog_bp
+from .social import bp as social_bp
+
+
+# Blueprints
+app_setup.register_blueprint(main_bp)
+app_setup.register_blueprint(recherches_bp)
+app_setup.register_blueprint(blog_bp)
+app_setup.register_blueprint(social_bp)
+
+
+# Create server & Link app to server
+app = app_setup.app
+server_setup = ServerSetup(_app_=app)
+
+
+# Run server
 def start_app(deployment: str = ''):
-    from flask import url_for
-    from .setups import app_setup
-    from .setups.server_setup import ServerSetup
-    from .main import bp as main_bp
-    from .recherches import bp as recherches_bp
-    from .blog import bp as blog_bp
-    from .social import bp as social_bp
-
-    # Blueprints
-    app_setup.register_blueprint(main_bp)
-    app_setup.register_blueprint(recherches_bp)
-    app_setup.register_blueprint(blog_bp)
-    app_setup.register_blueprint(social_bp)
-
-
-    # Create server & Link app to server
-    server_setup = ServerSetup(_app_=app_setup.app)
-
-    # Run server
     if deployment != '':
         server_setup.run_app(deploy_option=deployment)
     else:
--- APP/url_authenticater.py
+++ APP/url_authenticater.py
@@ -9,6 +9,7 @@ domains = [
     "http://192.168.0.129:8080",
     "http://127.0.0.1:8080",
     "http://localhost"
+    "http://greenpole.pythonanywhere.com/"
 ]
 
 endpoints = [
