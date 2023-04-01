83ba5fbb7083dc77cd2c1a522e0be543d81bf290
joseph <ahouantojoseph@gmail.com>
2023-04-01 14:21:39 -0400

    samedi 1er avril 2023 - J'ai terminé le formulaire pour l'ajout des articles.

--- APP/blog/endpoints.py
+++ APP/blog/endpoints.py
@@ -1,11 +1,29 @@
 from .setup import bp, render_page, variables, WEBSITE_NAME, secure_redirect
 from flask_login import login_user, login_required, logout_user, current_user
+from flask import flash
 from ..crud_ops import *
 from ..forms import *
 from ..models import *
 
 
 # -------- Pages' endpoints -------- #
+is_published = None
+messaged:int = 0
+def published(state):
+    global messaged
+    global is_published
+
+    if state is not None:
+        is_published = state
+
+    if state is None:
+        if messaged == 1:
+            is_published = None
+            messaged = 0
+        else:
+            messaged +=1
+        return is_published
+
 
 # La page home est une page servant à diriger les utilisateurs vers les autres pages de la section recherches.
 def get_all_articles():
@@ -42,16 +60,18 @@ def example_article():
 def editing():
     form = CreatePostForm()
     if form.validate_on_submit():
+        published(False)
         new_article = Article(
             title=form.title.data,
             content=form.body.data,
             image=form.img_url.data,
-            author_name="sexdcnhmyjb"
+            author_name=form.author.data
         )
         db.session.add(new_article)
         db.session.commit()
+        published(True)
         return secure_redirect("editing")
-    return render_page('editing', form=form, website_name=WEBSITE_NAME, add_navbar_footer=True, page_title="Editing", current_li=bp.name, disable_extra_part=(current_user.is_authenticated))
+    return render_page('editing', form=form, is_published=published(None), website_name=WEBSITE_NAME, add_navbar_footer=True, page_title="Editing", current_li=bp.name, disable_extra_part=(current_user.is_authenticated))
 
 # #
 
--- APP/forms.py
+++ APP/forms.py
@@ -7,16 +7,15 @@ from flask_ckeditor import CKEditorField
 
 # WTForm
 class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    author_name = StringField("Author", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    title = StringField("Blog Post Title:", validators=[DataRequired()])
+    author = StringField("Author:", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL:", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content:", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
     def reset_form(self):
         self.title.data = ''
-        self.subtitle.data = ''
+        self.author.data = ''
         self.img_url.data = ''
         self.body.data = ''
 
